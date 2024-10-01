c54424e1f88e3138346b096e834b4fc64aaf99e2
Sving1024 <piano5432@outlook.com>
2024-10-02 01:22:11 +0800

    fix actions

--- .github/workflows/config_ssh.sh
+++ .github/workflows/config_ssh.sh
@@ -4,16 +4,16 @@ GLOBIGNORE=".:.."
 ssh_keyscan_types='rsa,ecdsa,ed25519'
 
 echo '::group::Adding aur.archlinux.org to known hosts'
-ssh-keyscan -v -t "$ssh_keyscan_types" aur.archlinux.org >>~/.ssh/known_hosts
+ssh-keyscan -v -t "$ssh_keyscan_types" aur.archlinux.org >> /home/builder/.ssh/known_hosts
 echo '::endgroup::'
 
 echo '::group::Importing private key'
-echo "$1" >~/.ssh/aur
-chmod -vR 600 ~/.ssh/aur*
-ssh-keygen -vy -f ~/.ssh/aur >~/.ssh/aur.pub
+echo "$1" > /home/builder/.ssh/aur
+chmod -vR 600 /home/builder/.ssh/aur*
+ssh-keygen -vy -f /home/builder/.ssh/aur > /home/builder/.ssh/aur.pub
 echo '::endgroup::'
 
 echo '::group::Checksums of SSH keys'
-sha512sum ~/.ssh/aur ~/.ssh/aur.pub
-cat ~/.ssh/aur.pub
+sha512sum /home/builder/.ssh/aur /home/builder/.ssh/aur.pub
+cat /home/builder/.ssh/aur.pub
 echo '::endgroup::'
