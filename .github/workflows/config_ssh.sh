dfe738988bc889fa744884cdf84b53ab1c3dd750
Sving1024 <piano5432@outlook.com>
2024-10-02 00:38:36 +0800

    fix actions

---
+++ .github/workflows/config_ssh.sh
@@ -0,0 +1,18 @@
+# Ignore "." and ".." to prevent errors when glob pattern for assets matches hidden files
+GLOBIGNORE=".:.."
+
+ssh_keyscan_types='rsa,ecdsa,ed25519'
+
+echo '::group::Adding aur.archlinux.org to known hosts'
+ssh-keyscan -v -t "$ssh_keyscan_types" aur.archlinux.org >>~/.ssh/known_hosts
+echo '::endgroup::'
+
+echo '::group::Importing private key'
+echo "$1" >~/.ssh/aur
+chmod -vR 600 ~/.ssh/aur*
+ssh-keygen -vy -f ~/.ssh/aur >~/.ssh/aur.pub
+echo '::endgroup::'
+
+echo '::group::Checksums of SSH keys'
+sha512sum ~/.ssh/aur ~/.ssh/aur.pub
+echo '::endgroup::'
