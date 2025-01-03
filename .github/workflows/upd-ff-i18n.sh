d32bb8f62ee432998de152a5484b92cb5959a537
Sving1024 <sving1024@outlook.com>
2025-01-03 23:23:48 +0800

    fix shell script

--- .github/workflows/upd-ff-i18n.sh
+++ .github/workflows/upd-ff-i18n.sh
@@ -1,10 +1,10 @@
 echo $1
 echo $2
-local _url=https://ftp.mozilla.org/pub/firefox/nightly
-local _version=$(curl ${_url}/latest-mozilla-central-l10n/linux-x86_64/xpi/ | grep "\.${1}.langpack.xpi" | sed "s/^.*>firefox-//; s/\.$1.*//" | sort -n | tail -n 1)
-local _build_id_raw="$(curl -s "${_url}/latest-mozilla-central-l10n/firefox-${_version}.$1.linux-x86_64.checksums" | grep '.partial.mar' | cut -d' ' -f4 | grep -E -o '[[:digit:]]{14}' | sort | tail -n1)"
+_url=https://ftp.mozilla.org/pub/firefox/nightly
+_version=$(curl ${_url}/latest-mozilla-central-l10n/linux-x86_64/xpi/ | grep "\.${1}.langpack.xpi" | sed "s/^.*>firefox-//; s/\.$1.*//" | sort -n | tail -n 1)
+_build_id_raw="$(curl -s "${_url}/latest-mozilla-central-l10n/firefox-${_version}.$1.linux-x86_64.checksums" | grep '.partial.mar' | cut -d' ' -f4 | grep -E -o '[[:digit:]]{14}' | sort | tail -n1)"
 declare -A _build_id
-local _build_id=(
+_build_id=(
     [year]="${_build_id_raw:0:4}"
     [month]="${_build_id_raw:4:2}"
     [day]="${_build_id_raw:6:2}"
@@ -14,9 +14,9 @@ local _build_id=(
     [date]="${_build_id_raw:0:8}"
     [time]="${_build_id_raw:8:6}"
 )
-local _build_id_date=${_build_id[date]}
-local _build_id_time=${_build_id[time]}
-local pkgver=$(printf "%s.%s.%s" ${_version} ${_build_id_date} ${_build_id_time})
+_build_id_date=${_build_id[date]}
+_build_id_time=${_build_id[time]}
+pkgver=$(printf "%s.%s.%s" ${_version} ${_build_id_date} ${_build_id_time})
 echo "version=${_version}"
 echo "pkgver=${pkgver}"
 mkdir -p ./tmp/firefox-nightly-i18n-$1
